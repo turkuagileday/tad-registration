52120dadf735c4b5baf09e4cd125e374eede2e02
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2013-03-10 20:37:12 +0200

    Added experimental models

--- tad_registration/registration_app/models.py
+++ tad_registration/registration_app/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+class BillingType(models.Model):
+    y_id = models.CharField(max_length=255, blank=True) # y_id, ugh :(
+    recipient = models.CharField(max_length=255)
+    reference = models.CharField(max_length=255)
+
+class NormalBillingType(BillingType):
+    email_address = models.EmailField()
+
+class PostBillingType(BillingType):
+    extra_info = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    post_office = models.CharField(max_length=255)
+
+class EBillingType(BillingType):
+    address = models.CharField(max_length=255)
+    operator = models.CharField(max_length=255)
+
+
+class Registration(models.Model):
+    """
+    Reprecent single registration.
+    Every registration has contact_person, information about billing and all info about participants. Note that one registration can have more than one participant
+    """
+    BILLING_TYPE_CHOICES = (
+        ('email', 'Email'),
+        ('post', 'Post'),
+        ('ebilling', 'E-billing')
+    )
+    name = models.CharField(max_length=255)
+    organisation = models.CharField(max_length=255)
+    billing_type = models.CharField(max_length=1, choices=BILLING_TYPE_CHOICES)
+
+class Participant(models.Model):
+    PARTICIPATION_CHOICES = (
+        ('both_days', 'Both days'),
+        ('conference_day', 'Conference day'),
+        ('conference_day_member', 'Conference day (member)'),
+        ('student', 'Student')
+    )
+
+    T_SHIRT_SIZE = (
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+        ('XXL', 'XXL')
+    )
+
+    name = models.CharField(max_length=255)
+    participation_choice = models.CharField(max_length=1, choices=PARTICIPATION_CHOICES)
+    conference_dinner = models.CharField(max_length=1, choices=(('dinner', 'Dinner'),))
+    special_diet = models.CharField(max_length=255, blank=True)
+    t_shirt_size = models.CharField(max_length=1, choices=T_SHIRT_SIZE)
+    twitter_account = models.CharField(max_length=255, blank=True)
+# TODO: WORKSHOPS!
+    other_info = models.TextField(blank=True)
+
+    registration = models.ForeignKey(Registration)
+
+
