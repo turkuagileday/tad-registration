893582eadc6582d7a00fa2cc09a309f39af5cdfb
Janne Ala-Äijälä <janne@underdude.org>
2013-03-20 23:02:30 +0200

    Add parsley validation to the form. Mark required fields. Style validation errors.

--- tad_registration/registration_app/forms.py
+++ tad_registration/registration_app/forms.py
@@ -1,26 +1,38 @@
 from django.forms import ModelForm
 from models import Registration, Participant, NormalBillingType, PostBillingType, EBillingType
+from decorators import parsleyfy
 
+@parsleyfy
 class RegistrationForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = Registration
 
+
+@parsleyfy
 class ParticipantForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = Participant
         exclude = ('registration')
 
+@parsleyfy
 class NormalBillingForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = NormalBillingType
         exclude = ('registration')
 
+@parsleyfy
 class PostBillingForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = PostBillingType
         exclude = ('registration')
 
+@parsleyfy
 class EBillingForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = EBillingType
         exclude = ('registration')
