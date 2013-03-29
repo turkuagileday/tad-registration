ee25be3791e28cf73dbac5711f00ce3bfeeba3e4
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2013-03-29 02:55:17 +0200

    Add more fields to admin view

--- tad_registration/registration_app/admin.py
+++ tad_registration/registration_app/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from registration_app.models import Participant, Registration
 
-admin.site.register(Participant)
-admin.site.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+  list_display = ('organisation', 'contact_person', 'billing_type',)
+  list_filter = ('billing_type',)
+
+class ParticipantAdmin(admin.ModelAdmin):
+  list_display = ('name', 'participation_choice',)
+  list_filter = ('participation_choice',)
+
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Registration, RegistrationAdmin)
