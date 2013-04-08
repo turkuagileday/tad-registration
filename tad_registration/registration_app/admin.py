a60494e20ac799469fa1567756f8e8496a024097
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2013-04-08 21:02:45 +0300

    Add participant inline to registration admin view (for easily bulk adding participants)

--- tad_registration/registration_app/admin.py
+++ tad_registration/registration_app/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from registration_app.models import Participant, Registration
 
-class RegistrationAdmin(admin.ModelAdmin):
-  list_display = ('organisation', 'contact_person', 'billing_type',)
-  list_filter = ('billing_type',)
-
 class ParticipantAdmin(admin.ModelAdmin):
   list_display = ('name', 'participation_choice', 'conference_dinner',)
   list_filter = ('participation_choice', 'conference_dinner',)
+
+class ParticipantInline(admin.TabularInline):
+  model = Participant
+  extra = 0
+  can_delete = False
+  fields = ('name', 'participation_choice', 'conference_dinner', 't_shirt_size', 'twitter_account', 'email_address',)
+
+class RegistrationAdmin(admin.ModelAdmin):
+  inlines = [
+    ParticipantInline,
+  ]
+  list_display = ('organisation', 'contact_person', 'billing_type',)
+  list_filter = ('billing_type',)
 
 admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Registration, RegistrationAdmin)
